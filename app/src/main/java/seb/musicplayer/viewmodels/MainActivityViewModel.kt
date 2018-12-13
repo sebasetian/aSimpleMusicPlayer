110d7debe606314f682d9d25cdb68d9536f222f3
shih-yao <sebasetian123@gmail.com>
2018-12-12 18:23:48 -0800

    event, viewmodel,items

--- app/src/main/java/seb/musicplayer/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/seb/musicplayer/viewmodels/MainActivityViewModel.kt
@@ -1,10 +1,10 @@
 package seb.musicplayer.viewmodels
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.Transformations
-import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.*
+import android.util.Log
 import seb.musicplayer.dagger.MediaSessionConnection
+import seb.musicplayer.model.Event
+import seb.musicplayer.model.MediaItemData
 import javax.inject.Inject
 
 class MainActivityViewModel @Inject constructor(private val mediaSessionConnection: MediaSessionConnection
@@ -41,4 +41,49 @@ class MainActivityViewModel @Inject constructor(private val mediaSessionConnecti
             playMedia(clickedItem)
         }
     }
+
+    /**
+     * This posts a browse [Event] that will be handled by the
+     * observer in [MainActivity].
+     */
+    private fun browseToItem(mediaItem: MediaItemData) {
+        _navigateToMediaItem.value = Event(mediaItem.mediaId)
+    }
+
+    /**
+     * This method takes a [MediaItemData] and does one of the following:
+     * - If the item is *not* the active item, then play it directly.
+     * - If the item *is* the active item, check whether "pause" is a permitted command. If it is,
+     *   then pause playback, otherwise send "play" to resume playback.
+     */
+    fun playMedia(mediaItem: MediaItemData) {
+        val nowPlaying = mediaSessionConnection.nowPlaying.value
+        val transportControls = mediaSessionConnection.transportControls
+
+        val isPrepared = mediaSessionConnection.playbackState.value?.isPrepared ?: false
+        if (isPrepared && mediaItem.mediaId == nowPlaying?.id) {
+            mediaSessionConnection.playbackState.value?.let { playbackState ->
+                when {
+                    playbackState.isPlaying -> transportControls.pause()
+                    playbackState.isPlayEnabled -> transportControls.play()
+                    else -> {
+                        Log.w(TAG, "Playable item clicked but neither play nor pause are enabled!" +
+                                " (mediaId=${mediaItem.mediaId})")
+                    }
+                }
+            }
+        } else {
+            transportControls.playFromMediaId(mediaItem.mediaId, null)
+        }
+    }
+
+    class Factory(private val mediaSessionConnection: MediaSessionConnection
+    ) : ViewModelProvider.NewInstanceFactory() {
+
+        @Suppress("unchecked_cast")
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return MainActivityViewModel(mediaSessionConnection) as T
+        }
+    }
 }
+private const val TAG = "MainActivitytVM"
